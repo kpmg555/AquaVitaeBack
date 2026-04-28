fe50703d77ee6135e5d9b1d168400d39239d5849
FeryRi <a01656047@tec.mx>
2026-04-28 15:49:38 -0600

    Se modifica repositorio de  Alerta

--- src/main/java/com/aquavitae/domain/repository/AlertaRepository.java
+++ src/main/java/com/aquavitae/domain/repository/AlertaRepository.java
@@ -1,10 +1,7 @@
 package com.aquavitae.domain.repository;
 import com.aquavitae.domain.models.Alerta;
 
-import java.util.List;
 
 public interface AlertaRepository {
-    long contarAltasActivas();
-    long contarMediasActivas();
-    List<Alerta> findRecientes(int limite);
+    Alerta save(Alerta alerta);
 }
