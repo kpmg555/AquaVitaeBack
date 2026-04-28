997fb8851cdcfdef3a904cef61a3e523323de864
FeryRi <a01656047@tec.mx>
2026-04-27 21:25:36 -0600

    Se agrega mapper para las alertas

--- src/main/java/com/aquavitae/infrastructure/mapper/PlantaRiesgoMapper.java
+++ src/main/java/com/aquavitae/infrastructure/mapper/PlantaRiesgoMapper.java
@@ -1,4 +1,24 @@
 package com.aquavitae.infrastructure.mapper;
 
+import com.aquavitae.domain.models.PlantaRiesgo;
+import com.aquavitae.infrastructure.entities.PlantaEntity;
+
 public class PlantaRiesgoMapper {
+    public static PlantaRiesgo toDomain(PlantaEntity entity,
+                                        Float indiceActual,
+                                        Float indiceHace7Dias,
+                                        Boolean tieneAlertaCritica) {
+        PlantaRiesgo m = new PlantaRiesgo();
+        m.setId(entity.getId());
+        m.setNombre(entity.getNombre());
+        if (entity.getUbicacion() != null) {
+            m.setUbicacionNombre(entity.getUbicacion().getNombre());
+            m.setLatitud(entity.getUbicacion().getLatitud());
+            m.setLongitud(entity.getUbicacion().getLongitud());
+        }
+        m.setIndiceHidrico(indiceActual);
+        m.setIndiceHace7Dias(indiceHace7Dias);
+        m.setTieneAlertaCritica(tieneAlertaCritica != null && tieneAlertaCritica);
+        return m;
+    }
 }
