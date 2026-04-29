654b04c559b5d4a9d2b5f9fd9d2f0a2e3083dfe3
FeryRi <a01656047@tec.mx>
2026-04-28 19:39:17 -0600

    Se agregó modelo DashboardRiesgo

---
+++ src/main/java/com/aquavitae/domain/models/DashboardRiesgo.java
@@ -0,0 +1,17 @@
+package com.aquavitae.domain.models;
+
+import java.util.List;
+
+public class DashboardRiesgo {
+
+        private final List<PlantaRiesgo> plantas;
+        private final ResumenRiesgo resumen;
+
+        public DashboardRiesgo(List<PlantaRiesgo> plantas, ResumenRiesgo resumen) {
+            this.plantas = plantas;
+            this.resumen = resumen;
+        }
+
+        public List<PlantaRiesgo> getPlantas() { return plantas; }
+        public ResumenRiesgo getResumen() { return resumen; }
+}
