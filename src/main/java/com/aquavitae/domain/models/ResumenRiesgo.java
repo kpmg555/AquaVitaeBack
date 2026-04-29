0052df80953048777a2093af610a8716ca7ecc37
FeryRi <a01656047@tec.mx>
2026-04-28 19:37:11 -0600

    Se hicieron modificaciones a Planta RIesgo

---
+++ src/main/java/com/aquavitae/domain/models/ResumenRiesgo.java
@@ -0,0 +1,19 @@
+package com.aquavitae.domain.models;
+
+public class ResumenRiesgo {
+
+        private final long alto;
+        private final long medio;
+        private final long bajo;
+
+        public ResumenRiesgo(long alto, long medio, long bajo) {
+            this.alto = alto;
+            this.medio = medio;
+            this.bajo = bajo;
+        }
+
+        public long getAlto() { return alto; }
+        public long getMedio() { return medio; }
+        public long getBajo() { return bajo; }
+
+}
