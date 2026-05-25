355c5d55af54a2c674cac530bacd2cea154e6176
Carlos Olivarez <carmanolmon@gmail.com>
2026-05-25 16:38:44 -0600

    merge

--- src/main/java/com/aquavitae/domain/models/AuthenticatedUser.java
+++ src/main/java/com/aquavitae/domain/models/AuthenticatedUser.java
@@ -3,21 +3,50 @@ package com.aquavitae.domain.models;
 import java.util.List;
 
 public class AuthenticatedUser {
-
     private final String uid;
     private final String email;
+
+    private final Integer idEmpresa;
+    private final Integer idRol;
+    private final String fullName;
+
     private final String nombre;
     private final String apellido;
     private final String rol;
     private final List<String> permisos;
 
     public AuthenticatedUser(String uid, String email) {
-        this.uid = uid;
-        this.email = email;
-        this.nombre = null;
-        this.apellido = null;
-        this.rol = null;
-        this.permisos = List.of();
+        this(
+                uid,
+                email,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                List.of()
+        );
+    }
+
+    public AuthenticatedUser(
+            String uid,
+            String email,
+            Integer idEmpresa,
+            Integer idRol,
+            String fullName
+    ) {
+        this(
+                uid,
+                email,
+                idEmpresa,
+                idRol,
+                fullName,
+                null,
+                null,
+                null,
+                List.of()
+        );
     }
 
     public AuthenticatedUser(
@@ -28,8 +57,35 @@ public class AuthenticatedUser {
             String rol,
             List<String> permisos
     ) {
+        this(
+                uid,
+                email,
+                null,
+                null,
+                nombre != null && apellido != null ? nombre + " " + apellido : null,
+                nombre,
+                apellido,
+                rol,
+                permisos
+        );
+    }
+
+    public AuthenticatedUser(
+            String uid,
+            String email,
+            Integer idEmpresa,
+            Integer idRol,
+            String fullName,
+            String nombre,
+            String apellido,
+            String rol,
+            List<String> permisos
+    ) {
         this.uid = uid;
         this.email = email;
+        this.idEmpresa = idEmpresa;
+        this.idRol = idRol;
+        this.fullName = fullName;
         this.nombre = nombre;
         this.apellido = apellido;
         this.rol = rol;
@@ -42,6 +98,18 @@ public class AuthenticatedUser {
 
     public String getEmail() {
         return email;
+    }
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     public String getNombre() {
